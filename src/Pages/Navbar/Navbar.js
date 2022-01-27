7ab13b59540d1884136dc4bf482d528cf80bc035
Shoyeb Mohammed Suny <suny.w68@gmail.com>
2022-01-27 13:39:21 +0600

    nav

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -4,7 +4,6 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import useAuth from "../../context/useAuth";
